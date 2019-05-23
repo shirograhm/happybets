cc54d86bcee1114aba53cd20538c7e64750a89d1
xavierwg13 <xavier.graham@outlook.com>
2019-05-23 15:17:05 -0700

    Added model classes and organized code.

---
+++ happybets/happybets/Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  happybets
+//
+//  Created by Xavier Graham on 5/23/19.
+//  Copyright © 2019 Happy Bets LLC. All rights reserved.
+//
+
+import Foundation
+
+class UserModel {
+    
+}
