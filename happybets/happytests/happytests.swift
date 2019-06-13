9bfe44eeba6a6606ca9a2d23f726df0456901c80
xavierwg13 <xavier.graham@outlook.com>
2019-06-12 18:41:13 -0700

    OOps. Forgot to init firebase for tests.

--- happybets/happytests/happytests.swift
+++ happybets/happytests/happytests.swift
@@ -13,6 +13,9 @@ class happytests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // Configure Firebase app for testing
+        FirebaseApp.configure()
     }
 
     override func tearDown() {
